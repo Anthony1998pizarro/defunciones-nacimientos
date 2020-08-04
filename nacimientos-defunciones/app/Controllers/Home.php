58c7363c1d6b543141b691348895011b1c1b308a
andyponce <aponcec@est.ups.edu.ec>
2020-08-03 19:31:14 -0500

    template  index

--- nacimientos-defunciones/app/Controllers/Home.php
+++ nacimientos-defunciones/app/Controllers/Home.php
@@ -6,14 +6,22 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		$db = \Config\Database::connect();
-		$query= $db->query('select * from nivel_insti');
-		$result = $query->getResult();
-		foreach ($result as $row) {
-			echo $row->nivel_inst."<br/>"; 
-		}
+		// $db = \Config\Database::connect();
+		// $query= $db->query('select * from nivel_insti');
+		// $result = $query->getResult();
+		// foreach ($result as $row) {
+		// 	echo $row->nivel_inst."<br/>"; 
+		// }
+		return view('index_view');
 	}
-
+	public function home()
+	{
+		return view('index_view');
+	}
+	 public function about()
+	{
+		return view('index_view');
+	}
 	//--------------------------------------------------------------------
 
 }
---
+++ nacimientos-defunciones/app/Views/index_view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.css" media="screen">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="home">INEC CONSULTs</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<footer class="d-flex flex-column bg-dark text-white text-center">
+        <span>&copy; UPS - Gestion Base de Datos </span>
+
+        <ul class="list-unstyled">
+            <li>Anthony Pizarro</li>
+            <li>Andres Ponce</li>
+        </ul>
+    </footer>
+</body>
+</html>
